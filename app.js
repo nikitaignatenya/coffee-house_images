ca3bd7dffbe6515269646988d50787581978ff43
Nikita Ignatenya <ihnatenyawork@gmail.com>
2025-07-07 21:06:20 +0300

    dom

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 const cofee = document.getElementById("cofee");
 const tea = document.getElementById("tea");
 const dessert = document.getElementById("dessert");
-
-const cofeeFlex = document.querySelectorAll(
-  "main article .second_section .changeCartCofee .cofeeFlex"
+const butRet = document.querySelector(".return");
+const cartCofee = document.querySelector(
+  "main article .second_section .changeCartCofee"
 );
-const cofeeRemove = document.querySelectorAll(
-  "main article .second_section .changeCartCofee .cofeeRemove"
+const cartTea = document.querySelector(
+  "main article .second_section .changeCartTea"
 );
-
-const cofeeDelete1439 = document.querySelectorAll(
+const cartDes = document.querySelector(
+  "main article .second_section .changeCartDes"
+);
+const del1439 = document.querySelectorAll(
   "main article .second_section .changeCartCofee .cofee_delete_1439"
 );
-const cofeeDelete1089 = document.querySelectorAll(
+const del1089 = document.querySelectorAll(
   "main article .second_section .changeCartCofee .cofee_delete_1089"
 );
-
-const teaFlex = document.querySelectorAll(
-  "main article .second_section .changeCartTea .teaFlex"
-);
-
-const teaRemove = document.querySelectorAll(
-  "main article .second_section .changeCartTea .teaRemove"
-);
-
-const desRemove = document.querySelectorAll(
-  "main article .second_section .changeCartDes .desRemove"
-);
-
 const desDelete1439 = document.querySelectorAll(
   "main article .second_section .changeCartDes .des_delete_1439"
 );
@@ -37,78 +26,78 @@ const desDelete1089 = document.querySelectorAll(
 
 const media1439 = window.matchMedia("(max-width: 1439px)");
 const media1089 = window.matchMedia("(max-width: 1089px)");
+const media739 = window.matchMedia("(max-width: 739px)");
 
 cofee.addEventListener("click", () => {
-  cofeeFlex.forEach((el) => {
-    el.style.display = "flex";
-  });
-  desRemove.forEach((el) => {
-    el.style.display = "none";
-  });
-  teaRemove.forEach((el) => {
-    el.style.display = "none";
-  });
-
-  cofeeDelete1439.forEach((el) => {
-    if (!media1439.matches) el.style.display = "flex";
-  });
-  cofeeDelete1089.forEach((el) => {
-    if (!media1089.matches) el.style.display = "flex";
-  });
-  desDelete1439.forEach((el) => {
-    if (!media1439.matches) el.style.display = "none";
-  });
-  desDelete1089.forEach((el) => {
-    if (!media1089.matches) el.style.display = "none";
-  });
+  cartCofee.style.display = "flex";
+  cartTea.style.display = "none";
+  cartDes.style.display = "none";
 });
 
 tea.addEventListener("click", () => {
-  cofeeRemove.forEach((el) => {
-    el.style.display = "none";
-  });
-  desRemove.forEach((el) => {
-    el.style.display = "none";
-  });
-  teaRemove.forEach((el) => {
-    el.style.display = "flex";
-  });
-
-  cofeeDelete1439.forEach((el) => {
-    if (!media1439.matches) el.style.display = "none";
-  });
-  cofeeDelete1089.forEach((el) => {
-    if (!media1089.matches) el.style.display = "none";
-  });
-  desDelete1439.forEach((el) => {
-    if (!media1439.matches) el.style.display = "none";
-  });
-  desDelete1089.forEach((el) => {
-    if (!media1089.matches) el.style.display = "none";
-  });
+  cartCofee.style.display = "none";
+  cartTea.style.display = "flex";
+  cartDes.style.display = "none";
 });
 
 dessert.addEventListener("click", () => {
-  cofeeRemove.forEach((el) => {
-    el.style.display = "none";
-  });
-  desRemove.forEach((el) => {
+  cartCofee.style.display = "none";
+  cartTea.style.display = "none";
+  cartDes.style.display = "flex";
+});
+
+butRet.addEventListener("click", () => {
+  del1089.forEach((el) => {
     el.style.display = "flex";
   });
-  teaRemove.forEach((el) => {
-    el.style.display = "none";
-  });
-
-  cofeeDelete1439.forEach((el) => {
-    if (!media1439.matches) el.style.display = "none";
-  });
-  cofeeDelete1089.forEach((el) => {
-    if (!media1089.matches) el.style.display = "none";
+  del1439.forEach((el) => {
+    el.style.display = "flex";
   });
   desDelete1439.forEach((el) => {
-    if (!media1439.matches) el.style.display = "flex";
+    el.style.display = "flex";
   });
   desDelete1089.forEach((el) => {
-    if (!media1089.matches) el.style.display = "flex";
+    el.style.display = "flex";
   });
+  butRet.style.display = "none";
+});
+
+media739.addEventListener("change", (event) => {
+  if (!event.matches) butRet.style.display = "none";
+  else butRet.style.display = "flex";
+});
+
+media1089.addEventListener("change", (event) => {
+  if (event.matches) {
+    del1089.forEach((el) => {
+      el.style.display = "none";
+    });
+    desDelete1089.forEach((el) => {
+      el.style.display = "none";
+    });
+  } else {
+    del1089.forEach((el) => {
+      el.style.display = "flex";
+    });
+    desDelete1089.forEach((el) => {
+      el.style.display = "flex";
+    });
+  }
+});
+media1439.addEventListener("change", (event) => {
+  if (event.matches) {
+    del1439.forEach((el) => {
+      el.style.display = "none";
+    });
+    desDelete1439.forEach((el) => {
+      el.style.display = "none";
+    });
+  } else {
+    del1439.forEach((el) => {
+      el.style.display = "flex";
+    });
+    desDelete1439.forEach((el) => {
+      el.style.display = "flex";
+    });
+  }
 });
